9178cab33889acf50e739323b3937c6673057843
Indra Riksa <indrariksa@gmail.com>
2025-11-12 02:40:07 +0700

    Create latihanp6.dart

---
+++ latihanp6.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:live/bottom_navbar.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(useMaterial3: false),
+      home: DynamicBottomNavbar(),
+    );
+  }
+}
+
+class MyInput extends StatefulWidget {
+  const MyInput({super.key});
+
+  @override
+  State<MyInput> createState() => _MyInputState();
+}
+
+class _MyInputState extends State<MyInput> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Input Widget')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              decoration: const InputDecoration(
+                hintText: 'Write your name here...',
+                labelText: 'Your Name',
+              ),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              child: const Text('Submit'),
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(content: Text('Hello'));
+                  },
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
